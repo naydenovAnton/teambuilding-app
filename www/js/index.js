49c410be0b631ebdf58696bec662fea4a3fbf1a3
Anton Naydenov <anton@foresightprudence.com>
2014-07-10 22:06:03 +0300

    init

--- www/js/index.js
+++ www/js/index.js
@@ -13,43 +13,41 @@ var app = {
     },
     receivedEvent: function (id) {
         console.log('here');
-        alert('here');
         loadFile();
     }
 };
 
 function loadFile() {
-    alert('111');
-    console.log('1');
+    alert('1');
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
 }
 
 function gotFS(fileSystem) {
-    console.log('2');
+    alert('2');
     fileSystem.root.getFile("/sdcard/scavenger/data/config.txt", null, gotFileEntry, fail);
 }
 
 function gotFileEntry(fileEntry) {
-    console.log('3');
+    alert('3');
     fileEntry.file(gotFile, fail);
 }
 
 function gotFile(file){
-    console.log('4');
+    alert('4');
     readAsText(file);
 }
 
 function readAsText(file) {
-    console.log('5');
+    alert('5');
     var reader = new FileReader();
     reader.onloadend = function(evt) {
-        console.log("Read as text");
-        console.log(evt.target.result);
+        alert("Read as text");
+        alert(evt.target.result);
     };
     reader.readAsText(file);
 }
 
 function fail(evt) {
-    console.log('6');
-    console.log(evt.target.error.code);
+    alert('6');
+    alert(evt.target.error.code);
 }
