a9ca5c7706bb91cab162fec11f17529abf1220c4
Anton Naydenov <anton@foresightprudence.com>
2014-12-09 22:12:34 +0200

    Rejected commit: Default

--- www/js/index.js
+++ www/js/index.js
@@ -292,8 +292,8 @@ function createJson(text) {
     var unique = 0;
     $.each(json.images, function (key, image) {
 
-        var html = '<div class="rounded" style="width: 415px">';
-        html += '<img src="file:///mnt/sdcard/scavenger/images/' + image + '" width="415" />';
+        var html = '<div class="rounded" style="width: 410px">';
+        html += '<img src="file:///mnt/sdcard/scavenger/images/' + image + '" width="410" />';
         html += '<hr />';
         html += '<button onclick="capturePhotoWithFile(' + unique + ');">Снимай</button>';
         html += '<hr />';
